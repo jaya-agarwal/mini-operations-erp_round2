2c61e9a313a2ad8da929ab247e8e83064704a478
Dev <dev@example.com>
2026-09-11 18:12:00 +0000

    chore(frontend): wire ThemeProvider and ToastProvider into app root

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,14 +3,20 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 import { AuthProvider } from "./lib/auth";
+import { ThemeProvider } from "./lib/theme";
+import { ToastProvider } from "./lib/toast";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </BrowserRouter>
+    <ThemeProvider>
+      <ToastProvider>
+        <BrowserRouter>
+          <AuthProvider>
+            <App />
+          </AuthProvider>
+        </BrowserRouter>
+      </ToastProvider>
+    </ThemeProvider>
   </React.StrictMode>
 );
